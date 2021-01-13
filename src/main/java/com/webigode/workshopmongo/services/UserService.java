db1c6ae282716b45b8a4d3fb6a1ff68879d632f8
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-01-13 16:28:26 -0300

    Obtendo um usuário por id

--- src/main/java/com/webigode/workshopmongo/services/UserService.java
+++ src/main/java/com/webigode/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.webigode.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.webigode.workshopmongo.domain.User;
 import com.webigode.workshopmongo.repository.UserRepository;
+import com.webigode.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repository.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
 }
---
+++ src/main/java/com/webigode/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.webigode.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+}
