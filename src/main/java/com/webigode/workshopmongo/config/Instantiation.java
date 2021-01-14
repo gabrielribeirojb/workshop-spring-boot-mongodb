6ea358af62f4abd9c6f9492631cf902f9a3f262d
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-01-14 14:35:43 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/webigode/workshopmongo/config/Instantiation.java
+++ src/main/java/com/webigode/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package com.webigode.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.webigode.workshopmongo.domain.Post;
 import com.webigode.workshopmongo.domain.User;
+import com.webigode.workshopmongo.repository.PostRepository;
 import com.webigode.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -15,8 +19,14 @@ public class Instantiation implements CommandLineRunner{
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Autowired
+	private PostRepository postRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		userRepository.deleteAll();
 		
@@ -25,6 +35,13 @@ public class Instantiation implements CommandLineRunner{
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		postRepository.deleteAll();
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo, abraços.", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", maria);
+		
+		postRepository.saveAll(Arrays.asList(post1,post2));
 
 	}
 
---
+++ src/main/java/com/webigode/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.webigode.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.webigode.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
