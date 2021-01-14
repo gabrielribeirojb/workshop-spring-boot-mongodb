a9385a1744d40f03bdee8a4964436128d6003aed
Gabriel Ribeiro <gaabriel.jrb@gmail.com>
2021-01-14 14:56:15 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/webigode/workshopmongo/config/Instantiation.java
+++ src/main/java/com/webigode/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.webigode.workshopmongo.domain.Post;
 import com.webigode.workshopmongo.domain.User;
+import com.webigode.workshopmongo.dto.AuthorDTO;
 import com.webigode.workshopmongo.repository.PostRepository;
 import com.webigode.workshopmongo.repository.UserRepository;
 
@@ -38,8 +39,8 @@ public class Instantiation implements CommandLineRunner{
 		
 		postRepository.deleteAll();
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo, abraços.", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", maria);
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo, abraços.", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(post1,post2));
 
